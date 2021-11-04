beda87e10bcc0088ed0d590b4985bc97ba7d0626
Jennifer Clark <jennifer.c.schultz@gmail.com>
2021-11-03 21:42:48 -0600

    added draw_actors_action.py from teachers solution.

--- rfk/game/draw_actors_action.py
+++ rfk/game/draw_actors_action.py
@@ -1,3 +1,29 @@
 from game.action import Action
 
 # TODO: Define the DrawActorsAction class here
+# this working code comes from the teachers answers
+class DrawActorsAction(Action):
+    """A code template for drawing actors. The responsibility of this calss of objects
+    is is to use an output service to draw all actors on the screen.
+     
+     Stereotype : Controller
+     
+     Atrributes: _output_service (OutputService):  An instance of OutputService.
+     
+     """
+    def __init__(self, output_service):
+        """The class constructor. 
+        
+        Args: output_service (OutputService): An instance of OutputService.
+        
+        """
+        self._output_service = output_service
+        
+    def execute(self, cast):
+        """ Executes the action using the gien actors. 
+        Args: cast(dict): The game actors {key: tag, value: list}.
+        """
+        self._output_service.clear_screen()
+        for group in cast.values():
+            self._output_service.draw_actors(group)
+        self._output_service.flush_buffer()
